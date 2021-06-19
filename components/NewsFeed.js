f1467bfca82b0b62bc0fd560522cd7750556f98b
Jerry Varughese <jerryjacob97@gmail.com>
2021-06-19 17:57:59 -0600

    minor CSS updates

--- components/NewsFeed.js
+++ components/NewsFeed.js
@@ -1,7 +1,7 @@
 
 function NewsFeed({ articles }) {
     return (
-    <div className="px-5 sm:grid sm:grid-cols-2 lg:grid-cols-4 lg:px-10 ">
+    <div className="px-10 sm:grid sm:grid-cols-2 lg:grid-cols-4 lg:px-20">
         {articles.map((article,index) => (     
             <div key={index}
             className="cursor-pointer p-5 shadow-md space-y-1 hover:scale-105 transition duration-100 ease-in-out transform m-2"
--- components/Pagination.js
+++ components/Pagination.js
@@ -4,16 +4,16 @@ function Pagination({ page, url, total }) {
     const lastPage = Math.ceil(total/8)
     
     return (
-        <div className=" my-4 text-gray-100 hover:text-white flex items-center">
+        <div className="my-8 text-gray-100 hover:text-white flex items-center text-lg">
             {page > 1 && (
                 <Link href={`${url}?page=${page-1}`}>
-                    <a className="cursor-pointer bg-red-600 hover:bg-red-500 px-3 py-1 rounded-md mx-1">Prev</a>
+                    <a className="cursor-pointer bg-red-600 hover:bg-red-500 px-4 py-1 rounded-md mx-1">Prev</a>
                 </Link>
             )}
             <p className="px-2 text-gray-800">{page} / {lastPage}</p>
             {page < lastPage && (
                 <Link href={`${url}?page=${page+1}`}>
-                    <a className="cursor-pointer bg-red-600 hover:bg-red-500 px-3 py-1 rounded-md mx-1">Next</a>
+                    <a className="cursor-pointer bg-red-600 hover:bg-red-500 px-4 py-1 rounded-md mx-1">Next</a>
                 </Link>
             )}
         </div>
